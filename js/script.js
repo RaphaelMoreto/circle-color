bb4c7870e24b23c3697ce376ab461065dadf5034
Raphael Moreto <raphaelqmoreto@gmail.com>
2024-12-21 13:39:27 -0300

    ADICIONADO TAG E FUNÇÃO PARA HEXADECIMAL

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const circle = document.querySelector(".circle");
 const rgbRed = document.querySelector("#rgb-red");
 const rgbGreen = document.querySelector("#rgb-green");
 const rgbBlue = document.querySelector("#rgb-blue");
+const hexColor = document.querySelector("#hex-color");
 
 const redInput = document.getElementById("red");
 const greenInput = document.getElementById("green");
@@ -23,22 +24,6 @@ function updateNumbersValue() {
     rgbBlue.textContent = blue.value;
 }
 
-//ADICIONA UM OUVINTE NO ELEMENTO INPUT E EXECUTA A FUNÇÃO QUANDO ACONTECE A INTERAÇÃO
-redInput.addEventListener("input", () => {
-    updateCircleColor();
-    updateNumbersValue();
-});
-
-greenInput.addEventListener("input", () => {
-    updateCircleColor();
-    updateNumbersValue();
-});
-
-blueInput.addEventListener("input", () => {
-    updateCircleColor();
-    updateNumbersValue();
-});
-
 function updateBackGround() {
     const red = redInput.value;
     const green = greenInput.value;
@@ -47,3 +32,41 @@ function updateBackGround() {
 
     document.body.style.background = backgroundColor;
 }
+
+function updateHexColor() {
+    const red = redInput.value;
+    const green = greenInput.value;
+    const blue = blueInput.value;
+
+    const hex = rgbToHex(red, green, blue);
+    hexColor.textContent = hex;
+}
+
+function rgbToHex(r, g, b) {
+    const toHex = (value) => {
+        const hex = parseInt(value).toString(16);
+
+        //ADICIONA UM ZERO A ESQUERDA SE A STRING TIVER APENAS UM CARACTERE
+        return hex.padStart(2, "0");
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
+//ADICIONA UM OUVINTE NO ELEMENTO INPUT E EXECUTA A FUNÇÃO QUANDO ACONTECE A INTERAÇÃO
+redInput.addEventListener("input", () => {
+    updateCircleColor();
+    updateNumbersValue();
+    updateHexColor()
+});
+
+greenInput.addEventListener("input", () => {
+    updateCircleColor();
+    updateNumbersValue();
+    updateHexColor()
+});
+
+blueInput.addEventListener("input", () => {
+    updateCircleColor();
+    updateNumbersValue();
+    updateHexColor()
+});
